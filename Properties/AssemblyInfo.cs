d86ce64ca382be20e6a28d6c57bd87e2a0aefee7
practicaoit <practicaoit@SERNATUR1920062>
2019-12-13 00:10:32 -0300

    Sofia v2.5 Fix

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sofia Sernatur")]
 [assembly: AssemblyDescription("Software Oficial de Formulario de Informacion para Asesoramiento")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Oliver Osvaldo Consterla Araya")]
 [assembly: AssemblyProduct("Sofia")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
